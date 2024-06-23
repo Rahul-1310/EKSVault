6736a195f458df2101ddcd663fc3ec7b6c2a90ac
Rahul-1310 <55014295+Rahul-1310@users.noreply.github.com>
2024-06-23 18:49:02 +0530

    Update function.py

--- Lambda/function.py
+++ Lambda/function.py
@@ -1,1 +1,20 @@
 PH
+"source": ["aws.ec2"],
+"detail": (
+"awsRegion": ["eu-west-1"],
+"responseElements":
+"networkInterface":
+"networkInterfaceId": [1
+"exists": true
+new 126
+new 129
+"eventSource": ["ec2.amazonaws.com"],
+"eventName" : ["CreateNetworkInterface"],
+"userIdentity": l
+"sessionContext":
+"sessionIssuer": l
+"userName": ["AWSServiceRoleForAmazonEKS"]
+
+"request Parameters": l
+"description": ["Amazon EKS ‹your clustername›"]
+I
